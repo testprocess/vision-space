1e89de000d990f5b1092ca1b0c751ae46f487add
Dipokal HHJ <hhj@devent.kr>
2023-07-03 23:05:57 +0900

    init: threejs boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+
+const App: any = () => {
+    return (
+        <div>
+            <b>sdgsdg</b>
+
+
+        </div>
+    );
+};
+
+export default App;
