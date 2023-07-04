90e94b0b0fb8104fd2492d5f034da1998a9be02b
Dipokal HHJ <hhj@devent.kr>
2023-07-04 09:04:05 +0900

    feat: add plane mesh

--- src/three/Scene.ts
+++ src/three/Scene.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from "./jsm/OrbitControls.js";
 
 class Scene {
     scene: any
@@ -15,23 +16,28 @@ class Scene {
     async init() {
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color( 0x000000 );
-        this.scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
     
         const clock = new THREE.Clock();
     
     
         
         this.camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 100 );
-        this.camera.position.set( 0, 3, 7 );
+        this.camera.position.set( 0, 20, 10 );
+        this.camera.rotation.z = 90
+
         this.scene.add(this.camera);
     
     
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.shadowMap.enabled = true
+
+
     
         document.querySelector("#screen").appendChild( this.renderer.domElement );
         
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+
         const dirLight = new THREE.DirectionalLight( 0xffffff );
         dirLight.position.set( -40, 400, -70 );
         dirLight.shadow.camera.top = 150;
@@ -47,18 +53,34 @@ class Scene {
         hemiLight.position.set( 0, 120, 0 );
         this.scene.add(hemiLight);
         
-        const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: true} ) );
-        mesh.rotation.x = - Math.PI / 2;
-        mesh.receiveShadow = true;
-        this.scene.add(mesh);
-    
-    
-    
-        const helper = new THREE.CameraHelper( dirLight.shadow.camera );
-        this.scene.add( helper );
+        const meshFloor = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: true} ) );
+        meshFloor.rotation.x = - Math.PI / 2;
+        meshFloor.receiveShadow = true;
+        this.scene.add(meshFloor);
+
+
+        const meshFront = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0xffffff, depthWrite: true} ) );
+        meshFront.rotation.x = 0;
+        meshFront.position.z = -20
+        meshFront.receiveShadow = true;
+        this.scene.add(meshFront);
         
+        const meshLeft = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0xfff777, depthWrite: true} ) );
+        meshLeft.rotation.y =  Math.PI / 2;
+        meshLeft.position.x = -20
+        meshLeft.receiveShadow = true;
+        this.scene.add(meshLeft);
+
+        const meshRight = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ),new THREE.MeshPhongMaterial( { color: 0xfff777, depthWrite: true} ) );
+        meshRight.rotation.y = - Math.PI / 2;
+        meshRight.position.x = 20
+        meshRight.receiveShadow = true;
+        this.scene.add(meshRight);
+
         this.animate();
     }
+
+
 
     animate() {
         requestAnimationFrame( this.animate.bind(this) );
